b300ac6494bffec748fc2e7f796bde3a41537d7a
FlorianBetin <113387278+FlorianBetin@users.noreply.github.com>
2023-08-03 16:40:12 +0200

    1 file modified

--- src/Message.js
+++ src/Message.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Message() {
-  useEffect(() => {
-    console.log(`User is logged In`);
-  }, []);
 
   useEffect(() => {
     console.log(`User is logged In`);
-    return () => console.log('bye') // callback qui affiche bye quand le composant se démonte
+    return () => console.log('bye') // callback qui affiche 'bye' quand le composant se démonte, return () => = fonction de netoyage
   }, []);
 
   return <h1>User is logged in</h1>;
